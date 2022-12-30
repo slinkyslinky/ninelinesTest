de60cd945b224e369205c9cae42deadf89bc1b61
SlinkySlinky <slinkislinki@gmail.com>
2022-12-30 21:56:32 +0300

    added preloader

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,28 @@
 import './vendor';
 import './helpers';
 import './components/social';
-import {ieFix} from './vendor/ie-fix';
-import {vhFix} from './vendor/vh-fix';
-import {actualYear} from './modules/actualYear';
+import { ieFix } from './vendor/ie-fix';
+import { vhFix } from './vendor/vh-fix';
+import { actualYear } from './modules/actualYear';
 import header from './components/header';
 import lazyLoading from './modules/lazyLoading';
 import scrollToAnchor from './modules/scrollToAnchor';
+
+
 
 ieFix();
 vhFix();
 actualYear();
 scrollToAnchor.init();
 
+
 header.init();
 lazyLoading.init();
+
+window.addEventListener("DOMContentLoaded", () => {
+	const textFade = document.querySelector(".text-fade");
+	textFade.classList.add("text-fade--animated");
+	document.querySelector(".preloader").classList.add("preloader--closed")
+	document.querySelector("html").style.overflowY = "auto"
+})
+
